b582aa94c4c74c5de12e63c4d2a72d7caf5604b8
Giovanni Galatro <giovanni.galatro@ibm.com>
2022-04-26 18:34:41 +0200

    Fix image size

--- app/server.py
+++ app/server.py
@@ -11,26 +11,12 @@ token = os.environ['TOKEN']
 deployment_id = os.environ['DEPLOYMENT_ID']
 model_deployment_endpoint_url    = f'https://cpd-cpd-instance.itzroks-666000zj44-mn5nm2-4b4a324f027aea19c5cbc0c3275c4656-0000.eu-de.containers.appdomain.cloud/ml/v4/deployments/{deployment_id}/predictions?version=2022-04-21';
 
-def getRGBAArr(canvas_data):
+def createPayload( canvas_data ):
     import numpy as np
-    dimension = 14
     rgba_data = canvas_data['values'][0]
     rgba_arr  = np.asarray(rgba_data).astype('uint8')
-    return rgba_arr.reshape(dimension, dimension, 4)
-
-def getNormAlphaList(img):
-    import numpy as np
-    alpha_arr       = np.array(img.split()[-1])
-    norm_alpha_arr  = alpha_arr / 255
-    return norm_alpha_arr
-
-def createPayload( canvas_data ):
-    rgba_arr      = getRGBAArr(canvas_data)               # Create an array object with the required shape
-    img           = Image.fromarray(rgba_arr, 'RGBA')     # Create an image object that can be resized
-    sm_img        = img.resize((28, 28), Image.LANCZOS) # Resize the image to 28 x 28 pixels
-    alpha_list    = getNormAlphaList(sm_img)              # Create a 1 x 784 array of values between 0 and 1
-    alpha_list    = alpha_list.reshape((1,28,28))
-    model_payload = {"input_data": [{"values" : alpha_list.tolist()}]} 
+    rgba_arr = rgba_arr.reshape(1, 28, 28,1)
+    model_payload = {"input_data": [{"values" : rgba_arr.tolist()}]} 
     return model_payload
 
 app = Flask( __name__, static_url_path='' )
@@ -50,7 +36,6 @@ def sendtomodel():
             payload = createPayload(canvas_data)
             header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
             response_scoring = requests.post(model_deployment_endpoint_url, json=payload, headers=header, verify=False).text
-            print( "result: " + response_scoring)
             return json.dumps(json.loads(response_scoring)['predictions'][0])
         else:
             err = "Model endpoint URL not set in 'server.py'"
